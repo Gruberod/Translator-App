14005f2fa49338b8c8e828ce4235e5f0a4ec2b39
Daniela G <daniela.gruberova@merck.com>
2018-06-11 13:48:22 +0200

    basic elements added

--- App.js
+++ App.js
@@ -1,11 +1,29 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, Button, View } from 'react-native';
 
 export default class App extends React.Component {
+
+  state = {
+    toTranslate: '',
+    translation: '',
+    translationFound: false
+  }
+
+  translate = () => {
+    alert('translation in progress')
+  }
+  
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <TextInput
+        style={{marginTop: 150, width: 150, height: 40, borderColor: 'gray', borderWidth: 1}}
+        value=''
+      />
+        <Button
+          title="Translate"
+          onPress={this.translate}/>/>
       </View>
     );
   }
